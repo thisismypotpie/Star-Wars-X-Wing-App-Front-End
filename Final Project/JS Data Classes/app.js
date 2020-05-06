a37de25ffb0463ef3cbb083a3947246510ca412d
Brandon <bld@pdx.edu>
2020-05-06 08:20:47 -0700

    database accessed, now on to extraction

--- Final Project/JS Data Classes/app.js
+++ Final Project/JS Data Classes/app.js
@@ -1,7 +1,26 @@
-require('sqlite3').verbose();
+var sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
+var dbExists = fs.existsSync('./GameDB');
+if(dbExists)
+{
+/*let db = new sqlite3.Database('./GameDB', (err)=> {
+    if(err) {
+        console.log("Not able to connect to database.");
+    }
+    else
+    {
+        console.log("Connection established");
+    }
+});*/
+ let db = new sqlite3.Database('./GameDB', sqlite3.OPEN_READONLY);
+ db.serialize(function(){
+     db.all("SELECT * FROM ShipTable", function(err, allRows){
 
-function sayHello(name){
-    console.log("hello "+ name);
+        if(err != null){
+            console.log("There was an error retrieving ship data.");
+        }
+     })
+ }
+ 
+ )
 }
-
-sayHello("poop");
