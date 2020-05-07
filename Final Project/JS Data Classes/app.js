020a1cfc1984d90b56981825490bc2bb107d79c9
Brandon <bld@pdx.edu>
2020-05-07 13:53:03 -0700

    exercise 5 complete

--- Final Project/JS Data Classes/app.js
+++ Final Project/JS Data Classes/app.js
@@ -1,5 +1,15 @@
+
+//Requires
 var sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
+var ship = require('./Ship-Variants.js');
+var maneuver = require('./Maneuvers.js');
+
+//Data
+let ship_list = [];
+let maneuver_holder = [];
+
+
 var dbExists = fs.existsSync('../GameDB.db');
 if(dbExists)
 {
@@ -13,9 +23,17 @@ if(dbExists)
  });
  console.log("Connection Established");
 
+//Ship query
 db.all("SELECT * FROM ShipTable", function(err, tables){
-console.log(tables);
-});
+console.log(tables[0]);
+tables.forEach(element => {
+let maneuver_array = element.Manuevers.split('\n');
+console.log(maneuver_array);
+
+//ship_list.push(new ship(element.ShipType, element.Name, element.Attack, element.Agility, element.Shields, element.Hull, ));
+
+});//foreach loop
+});//ship query
 
  // close the database connection
 db.close((err) => {
