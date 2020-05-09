63dfd2e563e11b2863048704f6230c3ca6369153
Brandon <bld@pdx.edu>
2020-05-09 11:58:37 -0700

    exercise 7 complete

--- assign3/7-characters.js
+++ assign3/7-characters.js
@@ -1,3 +1,17 @@
 // Enter your code here
 
 const url = 'https://swapi.dev/api/people/';
+
+fetch(url)
+.then(response =>{return response.json();})
+.then(data =>{
+    //console.log(data.results);
+    data.results.forEach(element => {
+        console.log(element["name"]);
+        document.getElementById("results").innerHTML+="."+element["name"]+" - "+element["birth_year"]+"<br>";
+    });
+});
+
+fetch('https://swapi.dev/api/planets/')
+.then(response =>response.json())
+.then(data => console.log(data));
