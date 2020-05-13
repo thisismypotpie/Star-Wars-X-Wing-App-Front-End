a4ea05643791bb0da225968e9111a2d2005a4888
Brandon <bld@pdx.edu>
2020-05-13 15:06:05 -0700

    began work on pilot selection

--- Final Project/Pilot-Screen/Pilot-Screen.js
+++ Final Project/Pilot-Screen/Pilot-Screen.js
@@ -5,5 +5,21 @@ document.getElementById("back-button").addEventListener("click", function(){
     window.location.href = "../Upgrade-Screen/Upgrade-Screen.html";
   });
 
-  let chosen_ship = sessionStorage.getItem("chosenShip");
+  //Get the chosen ship and game data from the session storage.
+  let chosen_ship = sessionStorage.getItem("chosenShip");//[shipName, faction]
+  var game_data= JSON.parse(sessionStorage.getItem("game_data"));
   console.log(chosen_ship);
+  var display_pilots = [];
+
+//Get the each pilot of the chosen ship.
+game_data.all_pilots.forEach(pilot => {
+      console.log("Comparing: ");
+      if(pilot.ship_name.ship_name == chosen_ship[0]
+         && pilot.faction.toLowerCase() == chosen_ship[1].toLowerCase())
+      {
+        display_pilots.push(pilot);
+      }
+
+});
+console.log(display_pilots);
+
--- Final Project/Selection-Screen/Selection-Screen.js
+++ Final Project/Selection-Screen/Selection-Screen.js
@@ -77,7 +77,7 @@ for(var i =0; i < faction_options.length;i++)
             current_index_tab++;
             new_item.textContent = name;
             //When a ship item is clicked, move to the next form.
-            new_item.addEventListener("dblclick",function(){
+            new_item.addEventListener("click",function(){
               window.location.href = "../Pilot-Screen/Pilot-Screen.html";
               sessionStorage.setItem("chosenShip",[new_item.id,chosenFactionElement.id]);
             })
