5148de315cc1f937ea14b19c34ba50e6a73b6f54
Brandon <bld@pdx.edu>
2020-05-18 14:54:50 -0700

    added upgrade removal popup

--- Final Project/Upgrade-Screen/Upgrade-Screen.js
+++ Final Project/Upgrade-Screen/Upgrade-Screen.js
@@ -61,7 +61,32 @@ let grid_items = document.getElementsByClassName("grid-item");
 document.getElementById("next-selection").id = "empty";
 ship_in_progress.upgrades.forEach(upgrade =>{
    let element = document.getElementById("empty");
-   element.style.backgroundImage = "url('"+upgrade.image_path+"')"
+   if(upgrade.characteristics != null && upgrade.characteristics.includes("Dual"))//If it is a dual upgrade, show the first side of that upgrade.
+   {
+    element.style.backgroundImage = "url('"+upgrade.image_path.split("\n")[0]+"')";
+    element.style.border = "3px solid red";
+   }
+   else
+   {
+    element.style.backgroundImage = "url('"+upgrade.image_path+"')";
+   }
+   element.addEventListener("click",function(){ //When you click each taken upgrade, you will be asked if you want to delete the upgrade.
+      let overlay = document.getElementById("overlay");
+      let upgrade_removal_box = document.getElementById("upgrade-removal-box");
+      overlay.style.opacity = 1;
+      upgrade_removal_box.style.visibility = "visible";
+      overlay.style.pointerEvents = "all";
+      if(upgrade.characteristics != null && upgrade.characteristics.includes("Dual"))//If a dual sided upgrade is being deleted, split path to get only first side of upgrade.
+      {
+        document.getElementById("upgrade-photo").style.backgroundImage = "url('"+upgrade.image_path.split("\n")[0]+"')";
+        document.getElementById("upgrade-photo").style.border = "3px solid red";
+      }
+      else
+      {
+        document.getElementById("upgrade-photo").style.backgroundImage = "url('"+upgrade.image_path+"')";
+      }
+
+   })
    element.id = "taken";
 })
 if(ship_in_progress.upgrades.length <=12)
@@ -70,6 +95,18 @@ if(ship_in_progress.upgrades.length <=12)
 }
 
 let next_upgrade_slot = document.getElementById("next-selection");
+//click event for the next selection for upgrade slot with plus button on it.
 next_upgrade_slot.addEventListener("click",function(){
   window.location.href = "./upgrade-type-selection-screen/upgrade-type-selection-screen.html";
 })
+
+
+//If you press the no button when asked if you want to delete an upgrade.
+document.getElementById("no-button").addEventListener("click",function(){
+
+})
+//If you press the yes button when asked if you want to delete an upgrade.
+document.getElementById("yes-button").addEventListener("click",function(){
+
+})
+
