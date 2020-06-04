0139c4a09c8865607dea72d7a2e2e64440ee46ea
Brandon <bld@pdx.edu>
2020-06-04 14:16:00 -0700

    removed all homework to make this repo a final project only repo

--- assign3/3-count.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("find").addEventListener("click", function(){
-
-var search = document.getElementById("input").value;
-var text = document.getElementById("intro").textContent;
-text = text.replace(new RegExp(search,'g'),"<mark style=\"background-color:#FFFF00;\">"+search+"</mark>");
-document.getElementById("intro").innerHTML = text;
-});
