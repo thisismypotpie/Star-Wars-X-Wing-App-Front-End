a30720ec3989d737fb6174071985eaf87de8a22a
Brandon <bld@pdx.edu>
2020-06-24 21:44:13 -0700

    progress made on the ship sorting page, need to fix back button errors

--- Front-End-Files/Gameplay-Screens/Pilot-Skill-Sorting-Screen/Pilot-Skill-Sorting-Screen.js
+++ Front-End-Files/Gameplay-Screens/Pilot-Skill-Sorting-Screen/Pilot-Skill-Sorting-Screen.js
@@ -3,6 +3,7 @@ var buckets = JSON.parse(sessionStorage.getItem("buckets"));//List of all bucket
 var indecies = JSON.parse(sessionStorage.getItem("indecies"));//List of indecies in buckets that have been sorted.
 var all_teams = JSON.parse(sessionStorage.getItem("all_teams"));
 var unsorted_ship_box = document.getElementById("unsorted-ship-box");
+var sorted_ship_box = document.getElementById("sorted-ship-box");
 var selected_index = 0;
 
 //Start indecies as an empty array if indecies is null or undefined.
@@ -43,13 +44,18 @@ buckets[selected_index].roster_numbers.forEach(roster_num=>{//Go through each ro
          let ship = team.ship_list[team.ship_list.map(function(e){return e.roster_number}).indexOf(roster_num)];
          console.log(ship);
          var ship_image = document.createElement("div");
+         ship_image.className = "unsorted-ship-image";
          ship_image.style.backgroundImage = "url('"+ship.chosen_pilot.image_path+"')";
          ship_image.style.border = "1px solid white";
          ship_image.style.backgroundRepeat = "no-repeat";
          ship_image.style.backgroundSize = "100% 100%";
-         ship_image.style.width = "50%";
-         ship_image.style.height = "50vh";
+         ship_image.style.height = "65vh";
          ship_image.style.display = "block";
+         ship_image.style.flex = "0 0 35%";//Width is here.
+         ship_image.style.marginLeft = "3%";
+         ship_image.style.marginRight = "3%";
+         ship_image.setAttribute("roster-number",ship.roster_number);
+         ship_image.onclick = function(){unsorted_image_click(ship_image)};
          unsorted_ship_box.appendChild(ship_image);
       }
    })
@@ -60,9 +66,19 @@ buckets[selected_index].roster_numbers.forEach(roster_num=>{//Go through each ro
 //This will refresh the page so that the next bucket can be examined.
 function done_button_click()
 {
+    if(document.getElementsByClassName("unsorted-ship-image").length > 0)
+    {
+        alert("Please sort all ships before continuing.");
+    }
+    else
+    {
+        var roster_array = Array.from(document.getElementsByClassName("sorted-ship-image"));
+        buckets[selected_index].roster_numbers = roster_array.map(function(e){return e.getAttribute("roster-number")});
+        console.log(buckets[selected_index]);
         sessionStorage.setItem("buckets",JSON.stringify(buckets));
         sessionStorage.setItem("indecies",JSON.stringify(indecies));
         location.reload();
+    }
 }
 
 //Remove the most previous entry in indecies and go back to most previous bucket.
@@ -82,4 +98,20 @@ function back_button_click()
             location.reload();
         }
 
+}
+
+//Take image off of unsorted image list and put it in the sorted list.
+function unsorted_image_click(clicked_element)
+{
+    clicked_element.className = "sorted-ship-image";
+    clicked_element.onclick = function(){sorted_image_click(clicked_element)};
+    sorted_ship_box.appendChild(clicked_element);
+}
+
+//Take image off sorted image list and put it in the unsorted list.
+function sorted_image_click(clicked_element)
+{
+    clicked_element.className = "unsorted-ship-image";
+    clicked_element.onclick = function(){unsorted_image_click(clicked_element)};
+    unsorted_ship_box.appendChild(clicked_element);
 }
