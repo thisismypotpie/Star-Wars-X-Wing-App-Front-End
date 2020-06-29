7aad295e04c2e7bf57ae0f586b1eb85ee6ca1ebe
Brandon <bld@pdx.edu>
2020-06-28 19:57:37 -0700

    added token icons to the maneuver selection page

--- Front-End-Files/Gameplay-Screens/Maneuver-Selection-Screen/Maneuver-Sellection-Screen.js
+++ Front-End-Files/Gameplay-Screens/Maneuver-Selection-Screen/Maneuver-Sellection-Screen.js
@@ -1,6 +1,7 @@
 //Get data needed for this page.
 var all_teams = JSON.parse(sessionStorage.getItem("all_teams"));
-var selected_index = 0;
+var selected_ship_index = 0;
+var maneuver_index = 0;
 
 //Grab element needed to manipulate.
 var pilot_image = document.getElementById("pilot-image");
@@ -11,13 +12,17 @@ var agility_label = document.getElementById("agility-text");
 var hull_label = document.getElementById("hull-text");
 var shield_label = document.getElementById("shield-text");
 var energy_label = document.getElementById("energy-text");
+var maneuver_range_label = document.getElementById("maneuver-range");
+var maneuver_type_label = document.getElementById("maneuver-type");
 
 //Set the first pilot to select maneuver of  the first team.
-pilot_image.style.backgroundImage = "url("+all_teams[0].ship_list[selected_index].chosen_pilot.image_path+")";
-roster_number_label.innerText = all_teams[0].ship_list[selected_index].roster_number;
-pilot_skill_label.innerText = all_teams[0].ship_list[selected_index].current_pilot_skill;
-attack_label.innerText = all_teams[0].ship_list[selected_index].current_attack;
-agility_label.innerText = all_teams[0].ship_list[selected_index].current_agility;
-hull_label.innerText = all_teams[0].ship_list[selected_index].current_hull;
-shield_label.innerText = all_teams[0].ship_list[selected_index].current_sheilds;
-energy_label.innerText = all_teams[0].ship_list[selected_index].current_energy;
+pilot_image.style.backgroundImage = "url("+all_teams[0].ship_list[selected_ship_index].chosen_pilot.image_path+")";
+roster_number_label.innerText = all_teams[0].ship_list[selected_ship_index].roster_number;
+pilot_skill_label.innerText = all_teams[0].ship_list[selected_ship_index].current_pilot_skill;
+attack_label.innerText = all_teams[0].ship_list[selected_ship_index].current_attack;
+agility_label.innerText = all_teams[0].ship_list[selected_ship_index].current_agility;
+hull_label.innerText = all_teams[0].ship_list[selected_ship_index].current_hull;
+shield_label.innerText = all_teams[0].ship_list[selected_ship_index].current_sheilds;
+energy_label.innerText = all_teams[0].ship_list[selected_ship_index].current_energy;
+maneuver_range_label.style.backgroundImage = "url("+all_teams[0].ship_list[selected_ship_index].chosen_pilot.ship_name.maneuvers[maneuver_index].range_symbol_path+")";
+maneuver_type_label.style.backgroundImage = "url("+all_teams[0].ship_list[selected_ship_index].chosen_pilot.ship_name.maneuvers[maneuver_index].maneuver_symbol_path+")";
