e6ea6ef398c4e9bb023eb3fc63a7ad8b45944e54
Brandon <bld@pdx.edu>
2020-10-22 09:23:04 -0700

    adding new ship in game files have begun

---
+++ Front-End-Files/Gameplay-Screens/Add-New-Ship-In-Game/Pilot-Selection-In-Game/Pilot-Selection-In-Game.js
@@ -0,0 +1,186 @@
+var aft_showing = false;//This is a boolean for large ships if the aft is showing or not, to flip the image when the flip button is pressed.
+
+//Get the chosen ship and game data from the session storage.
+let chosen_ship = sessionStorage.getItem("chosenShip").split(',');//[shipName, faction, size]
+var game_data= JSON.parse(sessionStorage.getItem("game_data"));
+console.log(chosen_ship);
+var display_pilots = sort_pilots_for_viewing(chosen_ship[0],chosen_ship[1]);
+let selection_index = 0;//This will be the index that will determine which pilot is chosen.
+
+
+//display maneuvers and pilot card
+document.getElementById("pilot-image").style.backgroundImage = "url('"+display_pilots[selection_index].image_path+"')";
+document.getElementById("maneuver-image").style.backgroundImage = "url('"+display_pilots[selection_index].ship_name.card+"')";
+update_pilot_image();//Updates the image to see if the pilot is available or not.
+
+//create functionality for the flip button
+if(display_pilots[selection_index].ship_name.ship_type.toLowerCase() == "largetwocard")
+{
+  document.getElementById("flip-button").style.visibility = "visible";
+}
+
+function flip_button_click()
+{
+  if(aft_showing == false)
+  {
+    document.getElementById("pilot-image").style.backgroundImage = "url('"+display_pilots[selection_index].aft_card_path+"')";
+    aft_showing = true;
+  }
+  else
+  {
+    document.getElementById("pilot-image").style.backgroundImage = "url('"+display_pilots[selection_index].image_path+"')";
+    aft_showing = false;
+  }
+}
+
+//This function is a cluster of functions that will update the pilot image if that pilot is unique and is already taken. It will be triggered each time a pilot image appears or is changed on this screen.
+function update_pilot_image()
+{
+ var pilot_stats = is_pilot_available();
+ if(pilot_stats.pilot_available == false)
+ {
+   update_image_unavailable(pilot_stats);
+ }
+ else
+ {
+   update_image_available();
+ }
+}
+
+
+//This function will test to see if any other team has a unique pilot and will return false if the pilot is not available.
+function is_pilot_available()
+{
+  var pilot_stats = {
+    pilot_available:true,
+    team_that_has_pilot: undefined,
+    roster_number: undefined
+  };
+  var all_teams = JSON.parse(sessionStorage.getItem("all_teams"));
+  //Automatically return true if there are no established teams.
+  if(all_teams == null || all_teams == undefined || all_teams.length == 0)
+  {
+    return pilot_stats;
+  }
+  else
+  {
+    for(const team of all_teams)
+    {
+      for(const ship of team.ship_list)
+      {
+        if(ship.chosen_pilot.is_unique == true && 
+          ship.chosen_pilot.pilot_name == display_pilots[selection_index].pilot_name &&
+          display_pilots[selection_index].is_unique == true)
+        {
+          pilot_stats.pilot_available = false;
+          pilot_stats.team_that_has_pilot = team.team_name;
+          pilot_stats.roster_number = ship.roster_number;
+           return pilot_stats;
+        }
+      }
+    }
+    return pilot_stats;
+  }
+
+}
+
+//This will change the image of the pilot to display that the pilot is not available and who has that pilot. Also make select button invisible.
+function update_image_unavailable(pilot_details)
+{
+document.getElementById("select-button").style.visibility = "hidden";
+document.getElementById("pilot-image").style.backgroundImage = "linear-gradient(rgba(255,255,255,0.8), rgba(255,255,255,0.8)), url("+ display_pilots[selection_index].image_path+")";
+
+document.getElementById("team_name_assigned").textContent = pilot_details.team_that_has_pilot;
+document.getElementById("roster_number_assigned").textContent = "ROSTER NUMBER: "+pilot_details.roster_number;
+
+document.getElementById("unavailable").style.visibility = "visible";
+document.getElementById("assigned_to").style.visibility = "visible";
+document.getElementById("team_name_assigned").style.visibility = "visible";
+document.getElementById("roster_number_assigned").style.visibility = "visible";
+}
+
+//This function will update an image if a pilot is available to make sure the unavailable text is not visible. Also make select button visible.
+function update_image_available()
+{
+document.getElementById("select-button").style.visibility = "visible";
+document.getElementById("pilot-image").style.opacity = "linear-gradient(rgba(255,255,255,1), rgba(255,255,255,1)), url("+ display_pilots[selection_index].image_path+")";
+document.getElementById("unavailable").style.visibility = "hidden";
+document.getElementById("assigned_to").style.visibility = "hidden";
+document.getElementById("team_name_assigned").style.visibility = "hidden";
+document.getElementById("roster_number_assigned").style.visibility = "hidden";
+
+}
+
+function next_button_click()
+{
+  selection_index ++;
+  if(selection_index >= display_pilots.length)
+  {
+    selection_index = 0;
+  }
+  document.getElementById("pilot-image").style.backgroundImage = "url('"+display_pilots[selection_index].image_path+"')";
+  update_pilot_image();//Updates the image to see if the pilot is available or not.
+}
+
+function previous_button_click()
+{
+selection_index --;
+if(selection_index < 0)
+{
+  selection_index = display_pilots.length-1;
+}
+document.getElementById("pilot-image").style.backgroundImage = "url('"+display_pilots[selection_index].image_path+"')";
+update_pilot_image();//Updates the image to see if the pilot is available or not.
+}
+
+function back_button_click()
+{
+sessionStorage.removeItem("chosenShip");
+window.location.href = "../Ship-Selection-In-Game/Ship-Selection-In-Game.html";
+}
+
+function select_button_click()
+{
+    //Create a new ship in game dependent on the size of the ship to determine what kind of in-game-ship needs to be delcared.
+    let team_name = JSON.parse(sessionStorage.getItem("new_team")).team_name;
+    if(!display_pilots[selection_index].ship_name.ship_type.toLowerCase().includes("large"))
+    {
+      sessionStorage.setItem("ship_in_progress",JSON.stringify(new in_game_ship_status(display_pilots[selection_index],team_name)));
+    }
+    else// if the ship is large, delacre the correct type of large in-game ship.
+    {
+      if(display_pilots[selection_index].ship_name.ship_type.toLowerCase() == "largeonecard")//large ship one card.
+      {
+        sessionStorage.setItem("ship_in_progress",JSON.stringify(new large_one_card_in_game_ship_status(display_pilots[selection_index],team_name)));
+      }
+      else if(display_pilots[selection_index].ship_name.ship_type.toLowerCase() == "largetwocard")//large ship two card.
+      {
+        sessionStorage.setItem("ship_in_progress",JSON.stringify(new large_two_card_in_game_ship_status(display_pilots[selection_index],team_name)));
+      }
+      else
+      {
+        console.log("ERROR: The ship size of the selected ship is not valid.");
+      }
+    }
+    window.location.href = "../Upgrade-Selection-In-Game/Upgrade-Selection-In-Game.html";
+}
+
+//Key bindings for this screen.
+document.onkeyup = function(e) {
+if(e.keyCode == 13)
+{
+  select_button_click();
+}
+else if(e.keyCode == 39)
+{
+  next_button_click();
+}
+else if(e.keyCode == 37)
+{
+  previous_button_click();
+}
+else if(e.keyCode == 27)
+{
+  back_button_click();
+}
+}
---
+++ Front-End-Files/Gameplay-Screens/Add-New-Ship-In-Game/Upgrade-Selection-In-Game/Upgrade-Selection-In-Game.js
@@ -0,0 +1,299 @@
+var flipped = false;//keeps track if front or back or large ship is showing.
+
+
+  //Get data objects needed for this page.
+  let ship_in_progress = JSON.parse(sessionStorage.getItem("ship_in_progress"));
+  var game_data= JSON.parse(sessionStorage.getItem("game_data"));
+  console.log(ship_in_progress);
+//Set pilot image of chosen pilot.
+document.getElementById("pilot-picture").style.backgroundImage = "url('"+ship_in_progress.chosen_pilot.image_path+"')";
+
+
+//I will be making a loop that will paste all of the upgrades a ship into the next available picture and then set the next empty to next selection.
+let grid_items = document.getElementsByClassName("grid-item");
+document.getElementById("next-selection").id = "empty";
+ship_in_progress.upgrades.forEach(upgrade =>{
+   let element = document.getElementById("empty");
+   if(upgrade.upgrade.is_dual_sided == true)//If it is a dual upgrade, show the first side of that upgrade.upgrade.
+   {
+    element.style.backgroundImage = "url('"+upgrade.upgrade.image_path.split("\n")[0]+"')";
+    element.style.border = "3px solid red";
+   }
+   else
+   {
+    element.style.backgroundImage = "url('"+upgrade.upgrade.image_path+"')";
+   }
+   //Add orndance tokens if an upgrade has them.
+   if(upgrade.ordnance_tokens > 0)
+   {
+     if(document.getElementById("ordnance-token-quantity") != null)
+     {
+       document.getElementById("ordnance-token-quantity");
+     }
+     var ordnance_token_quantity = document.createElement("div");
+     ordnance_token_quantity.id = "ordnance-token-quantity"
+     ordnance_token_quantity.style.gridRow = "1";
+     ordnance_token_quantity.style.gridColumn = "2";
+     ordnance_token_quantity.style.backgroundImage = "url('https://i.imgur.com/DztMvcD.png')";
+     ordnance_token_quantity.style.backgroundRepeat = "no-repeat";
+     ordnance_token_quantity.style.backgroundSize = "100% 100%";
+     ordnance_token_quantity.textContent = "X"+upgrade.ordnance_tokens;
+     ordnance_token_quantity.style.fontSize = "xx-large";
+     ordnance_token_quantity.style.fontFamily = "Impact, Charcoal, sans-serif";
+     ordnance_token_quantity.style.textAlign = "right"
+     ordnance_token_quantity.style.color = "white";
+     element.appendChild(ordnance_token_quantity);
+   }
+
+   element.addEventListener("click",function(){ //When you click each taken upgrade, you will be asked if you want to delete the upgrade.upgrade.
+      let overlay = document.getElementById("overlay");
+      let upgrade_removal_box = document.getElementById("upgrade-removal-box");
+      overlay.style.opacity = 1;
+      upgrade_removal_box.style.visibility = "visible";
+      overlay.style.pointerEvents = "all";
+      if(upgrade.upgrade.is_dual_sided == true)//If a dual sided upgrade is being deleted, split path to get only first side of upgrade.upgrade.
+      {
+        document.getElementById("upgrade-photo").style.backgroundImage = "url('"+upgrade.upgrade.image_path.split("\n")[0]+"')";
+        document.getElementById("upgrade-photo").style.border = "3px solid red";
+        document.getElementById("flip-button").style.visibility = "visible";
+      }
+      else
+      {
+        document.getElementById("upgrade-photo").style.backgroundImage = "url('"+upgrade.upgrade.image_path+"')";
+        document.getElementById("upgrade-photo").style.border = "1px solid white";
+      }
+      //Checks to see if the upgrade you are bringing up is in the right category to have ordnance tokens.
+      if(upgrade.upgrade.type == "Bombs" || upgrade.upgrade.type == "Torpedoes" || upgrade.upgrade.type == "Missiles")
+      {
+         document.getElementById("ordnance-upgrade-container").style.visibility = "visible";
+
+         //Set the buttons so the correct upgrade stats are displayed and can be altered.
+         document.getElementById("token-quantity").textContent = "X"+upgrade.ordnance_tokens;
+         document.getElementById("subtract-ordnance-token").onclick =function(){subract_ordnance_token(upgrade)};
+         document.getElementById("add-ordnance-token").onclick =function(){add_ordnance_token(upgrade)};
+
+         //Show number of ordnance tokens on each affected upgrades.
+         if(upgrade.ordnance_tokens > 0)
+         {
+          if(document.getElementById("ordnance-token-quantity") != null)
+          {
+            document.getElementById("ordnance-token-quantity");
+          }
+           var ordnance_token_quantity = document.createElement("div");
+           ordnance_token_quantity.style.gridRow = "1";
+           ordnance_token_quantity.style.gridColumn = "2";
+           ordnance_token_quantity.style.backgroundImage = "url('https://i.imgur.com/DztMvcD.png')";
+           ordnance_token_quantity.style.backgroundRepeat = "no-repeat";
+           ordnance_token_quantity.style.backgroundSize = "100% 100%";
+           ordnance_token_quantity.textContent = "X"+upgrade.ordnance_tokens;
+           ordnance_token_quantity.style.fontSize = "xx-large";
+           ordnance_token_quantity.style.fontFamily = "Impact, Charcoal, sans-serif";
+           ordnance_token_quantity.style.textAlign = "right"
+           ordnance_token_quantity.style.color = "white";
+           element.appendChild(ordnance_token_quantity);
+         }
+      }
+      else
+      {
+         document.getElementById("ordnance-upgrade-container").style.visibility = "hidden";
+      }
+      document.getElementById("upgrade-photo").setAttribute('name', upgrade.upgrade.name);//This is so when a user presses yes to delete, we can get the name of the upgrade.upgrade.
+   })
+   element.id = "taken";
+})
+if(ship_in_progress.upgrades.length <=12)
+{
+  document.getElementById("empty").id = "next-selection";
+}
+
+let next_upgrade_slot = document.getElementById("next-selection");
+//click event for the next selection for upgrade slot with plus button on it.
+
+next_upgrade_slot.addEventListener("click",function(){
+  window.location.href = "./Upgrade-Type-In-Game/Upgrade-Type-In-Game.html";
+})
+
+function close_remove_upgrade_pop_up()
+{
+  let overlay = document.getElementById("overlay");
+  let upgrade_removal_box = document.getElementById("upgrade-removal-box");
+  overlay.style.opacity = 0;
+  upgrade_removal_box.style.visibility = "hidden";
+  overlay.style.pointerEvents = "none";
+  document.getElementById("flip-button").style.visibility = "hidden";
+  document.getElementById("ordnance-upgrade-container").style.visibility = "hidden";
+  sessionStorage.setItem("ship_in_progress",JSON.stringify(ship_in_progress));
+  location.reload();
+}
+
+
+function remove_upgrade()
+{
+    let overlay = document.getElementById("overlay");
+    let upgrade_removal_box = document.getElementById("upgrade-removal-box");
+   overlay.style.opacity = 0;
+   upgrade_removal_box.style.visibility = "hidden";
+   overlay.style.pointerEvents = "none";
+   document.getElementById("flip-button").style.visibility = "hidden";
+   document.getElementById("ordnance-upgrade-container").style.visibility = "hidden";
+   //get the name of the upgrade, remove it from the ship's list of upgrades, then reload the page.
+   let upgrade_name = document.getElementById("upgrade-photo").getAttribute("name");
+   for(var i =0; i < ship_in_progress.upgrades.length;i++)
+   {
+     if(ship_in_progress.upgrades[i].upgrade.name == upgrade_name)
+     {
+      ship_in_progress.upgrades.splice(i,1);
+      break;
+     }
+   }
+    sessionStorage.setItem("ship_in_progress",JSON.stringify(ship_in_progress));
+   window.location.reload();
+}
+
+
+//Check if flip button is visible or not.
+if(ship_in_progress.chosen_pilot.ship_name.ship_type == "largeTwoCard")
+{
+  document.getElementById('main-flip-button').style.visibility = "visible";
+}
+
+function flip_button_click()
+{
+    let upgrade_name = document.getElementById("upgrade-photo").getAttribute("name");
+    for(var i =0; i < ship_in_progress.upgrades.length;i++)
+    {
+      if(ship_in_progress.upgrades[i].upgrade.name == upgrade_name)
+      {
+        if(ship_in_progress.upgrades[i].orientation == "front")
+        {
+          ship_in_progress.upgrades[i].orientation = "back";
+          document.getElementById("upgrade-photo").style.backgroundImage = "url('"+ship_in_progress.upgrades[i].upgrade.image_path.split("\n")[1]+"')";
+
+        }
+        else
+        {
+          ship_in_progress.upgrades[i].orientation = "front";
+          document.getElementById("upgrade-photo").style.backgroundImage = "url('"+ship_in_progress.upgrades[i].upgrade.image_path.split("\n")[0]+"')";
+        }
+        break;
+      }
+    }
+}
+
+function flip_ship(){
+  if(flipped == false)
+  {
+    document.getElementById('pilot-picture').style.backgroundImage = "url('"+ship_in_progress.chosen_pilot.aft_card_path+"')";
+    flipped = true;
+  }
+  else
+  {
+    document.getElementById('pilot-picture').style.backgroundImage = "url('"+ship_in_progress.chosen_pilot.image_path+"')";
+    flipped = false;
+  }
+}
+
+
+function back_button_push()
+{
+  sessionStorage.removeItem("ship_in_progress");
+  window.location.href = "../Pilot-Selection-In-Game/Pilot-Selection-In-Game.html";
+}
+
+function done_button_push()
+{
+  let overlay = document.getElementById("overlay");
+  let roster_number_box = document.getElementById("roster-number-box");
+  overlay.style.opacity = 1;
+  roster_number_box.style.visibility = "visible";
+  overlay.style.pointerEvents = "all";
+  document.getElementById("roster-number-input").focus();
+}
+
+function ok_button_push()
+{
+    //Add roster number to ship and add new team to all teams.
+      //Validate input and then add roster number to ship in progress.
+      if(!/^[0-9]+$/.test(document.getElementById("roster-number-input").value))
+      {
+         alert("Please only input positive numbers. No other input will be accepted.");
+         document.getElementById("roster-number-input").value = "";
+         return;
+      }
+      else
+      {
+         ship_in_progress.roster_number = parseInt(document.getElementById("roster-number-input").value,10);
+      }
+      // Add ship to the new team, then add the new team to all teams.
+      let new_team = JSON.parse(sessionStorage.getItem("new_team"));
+      new_team.ship_list.push(ship_in_progress);
+      let all_teams = JSON.parse(sessionStorage.getItem("all_teams"));
+      all_teams.push(new_team);
+      console.log(all_teams);
+      sessionStorage.setItem("all_teams", JSON.stringify(all_teams));
+
+      //remove all items that are no longer being used.
+      sessionStorage.removeItem("chosenShip");
+      sessionStorage.removeItem("new_team");
+      sessionStorage.removeItem("ship_in_progress");
+      window.location.href = "../../Maneuver-Selection-Screen/Maneuver-Sellection-Screen.js";
+
+      var dual_card_back_showing = false; //This is used for if the flip button shows up, if false showing front, if true, showing back
+}
+
+function close_button_push()//close the roster number pop-up.
+{
+  let overlay = document.getElementById("overlay");
+  let roster_number_box = document.getElementById("roster-number-box");
+  document.getElementById("roster-number-input").value = "";
+  overlay.style.opacity = 0;
+  roster_number_box.style.visibility = "hidden";
+  overlay.style.pointerEvents = "none";
+
+}
+
+function subract_ordnance_token(upgrade)
+{
+  if(upgrade.ordnance_tokens > 0)
+  {
+    upgrade.ordnance_tokens--;
+
+  }
+  else
+  {
+    upgrade.ordnance_tokens = 0;
+  }
+  document.getElementById("token-quantity").textContent = "X"+upgrade.ordnance_tokens;
+}
+function add_ordnance_token(upgrade)
+{
+    upgrade.ordnance_tokens++;
+    document.getElementById("token-quantity").textContent = "X"+upgrade.ordnance_tokens;
+}
+
+//Key bindings
+document.onkeyup = function(e) {
+if(document.getElementById("roster-number-box").style.visibility == "visible")
+{ 
+  if(e.keyCode == 13)//enter
+  {
+    ok_button_push();
+  }
+  else if(e.keyCode == 27)//escape key
+  {
+    close_button_push();
+  } 
+}
+else
+{
+  if(e.keyCode == 13)//enter
+  {
+    done_button_push();
+  }
+  else if(e.keyCode == 27)//escape key
+  {
+    back_button_push();
+  }
+}
+}
+
