d68aa07630f2d47ef606e6f0fb7ee4d37a8378ed
Brandon <bld@pdx.edu>
2020-11-03 17:27:58 -0800

    starting to add functionality to setup page

--- Front-End-Files/Galactic-Conquest-Screens/Setup-Screen/Setup-Screen.js
+++ Front-End-Files/Galactic-Conquest-Screens/Setup-Screen/Setup-Screen.js
@@ -1,4 +1,68 @@
+var gc_setup_data = {
+    faction_chosen:"",
+    resources_chosen:"",
+    planet_count:5,
+    pirate_faction:"",
+    planet_assignment:"",
+    location:""
+  };
+
 function go_to_main_menu()
 {
+    let game_data = JSON.parse(sessionStorage.getItem("game_data"));
+    sessionStorage.clear();
+    sessionStorage.setItem("game_data",JSON.stringify(game_data));
     window.location.href = "../../Title-Screen(Main Menu)/index.html";
+}
+
+function button_blur_for_pirates()
+{
+    document.getElementById("pirate-options-button").style.opacity = "0.3";
+    document.getElementById("pirate-options-button").onclick = null;
+}
+
+function button_focus_for_pirates()
+{
+    document.getElementById("pirate-options-button").style.opacity = "1.0";
+    document.getElementById("pirate-options-button").onclick = function(){piriate_button_click();};
+}
+
+function piriate_button_click()
+{
+    alert("pirate click")
+}
+
+function button_blur_for_planet_assignment()
+{
+    document.getElementById("planet-assignment-button").style.opacity = "0.3";
+    document.getElementById("planet-assignment-button").onclick = null;
+}
+
+function button_focus_for_planet_assignment()
+{
+    document.getElementById("planet-assignment-button").style.opacity = "1.0";
+    document.getElementById("planet-assignment-button").onclick = function(){planet_assignment_click()};
+}
+
+function planet_assignment_click()
+{
+    alert("planet click")
+}
+
+function rebel_faction_click()
+{
+    document.getElementById("rebel-faction-image").style.border = "2px solid white";
+    document.getElementById("rebel-faction-image").style.opacity = "1.0";
+    document.getElementById("imperial-faction-image").style.border = "none";
+    document.getElementById("imperial-faction-image").style.opacity = "0.3";
+    gc_setup_data.faction_chosen = "Rebels";
+}
+
+function imperial_faction_click()
+{
+    document.getElementById("imperial-faction-image").style.border = "2px solid white";
+    document.getElementById("imperial-faction-image").style.opacity = "1.0";
+    document.getElementById("rebel-faction-image").style.border = "none";
+    document.getElementById("rebel-faction-image").style.opacity = "0.3";
+    gc_setup_data.faction_chosen = "Imperial";
 }
