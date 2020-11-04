d030df219ebbd0e49948b94e7814d2a53b128a79
Brandon <bld@pdx.edu>
2020-11-04 11:18:48 -0800

    added pirate option functionality

--- Front-End-Files/Galactic-Conquest-Screens/Setup-Screen/Pirate-Options-Screen/Pirate-Options.js
+++ Front-End-Files/Galactic-Conquest-Screens/Setup-Screen/Pirate-Options-Screen/Pirate-Options.js
@@ -1,3 +1,13 @@
 function go_back_to_gc_setup(){
     window.location.href = "../Setup-Screen.html";
 }
+
+function add_ship_button_click(id)
+{
+
+}
+
+function subtract_ship_button_click(id)
+{
+    
+}
