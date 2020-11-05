f94d7b86de557ff5ea06a8d3844ad7b2f6b7d743
Brandon <bld@pdx.edu>
2020-11-04 18:55:22 -0800

    pirate options done

--- Front-End-Files/Galactic-Conquest-Screens/Setup-Screen/Pirate-Options-Screen/Pirate-Options.js
+++ Front-End-Files/Galactic-Conquest-Screens/Setup-Screen/Pirate-Options-Screen/Pirate-Options.js
@@ -18,8 +18,49 @@ var pirate_ship_quantities = {
     YT_1300:0,
     C_ROC_Cruiser:0
 }
+if(sessionStorage.getItem("pirate-ship-quantities")!=null)
+{
+    let pirate_data = JSON.parse(sessionStorage.getItem("pirate-ship-quantities"));
+    document.getElementById("ship-number-1").textContent = pirate_data.HWK_290;
+    update_token_quantity(1)
+    document.getElementById("ship-number-2").textContent = pirate_data.Kihraxz_Fighter;
+    update_token_quantity(2)
+    document.getElementById("ship-number-3").textContent = pirate_data.M3_A_Interceptor;
+    update_token_quantity(3)
+    document.getElementById("ship-number-4").textContent = pirate_data.M12_L_Kimongila_Fighter;
+    update_token_quantity(4)
+    document.getElementById("ship-number-5").textContent = pirate_data.G_1A_Starfighter;
+    update_token_quantity(5)
+    document.getElementById("ship-number-6").textContent = pirate_data.Protectorate_Starfighter;
+    update_token_quantity(6)
+    document.getElementById("ship-number-7").textContent = pirate_data.Quadjumper;
+    update_token_quantity(7)
+    document.getElementById("ship-number-8").textContent = pirate_data.Scurrg_H_6_Bomber;
+    update_token_quantity(8)
+    document.getElementById("ship-number-9").textContent = pirate_data.StarViper;
+    update_token_quantity(9)
+    document.getElementById("ship-number-10").textContent = pirate_data.Y_Wing;
+    update_token_quantity(10)
+    document.getElementById("ship-number-11").textContent = pirate_data.Z_95_Headhunter;
+    update_token_quantity(11)
+    document.getElementById("ship-number-12").textContent = pirate_data.Firespray_31;
+    update_token_quantity(12)
+    document.getElementById("ship-number-13").textContent = pirate_data.Hounds_Tooth;
+    update_token_quantity(13)
+    document.getElementById("ship-number-14").textContent = pirate_data.Aggressor;
+    update_token_quantity(14)
+    document.getElementById("ship-number-15").textContent = pirate_data.Jump_Master_5000;
+    update_token_quantity(15)
+    document.getElementById("ship-number-16").textContent = pirate_data.Lancer_Class_Pusuit_Craft;
+    update_token_quantity(16)
+    document.getElementById("ship-number-17").textContent = pirate_data.YT_1300;
+    update_token_quantity(17)
+    document.getElementById("ship-number-18").textContent = pirate_data.C_ROC_Cruiser;
+    update_token_quantity(18)
+}
 
 function go_back_to_gc_setup(){
+    sessionStorage.setItem("pirate-ship-quantities",JSON.stringify(pirate_ship_quantities))
     window.location.href = "../Setup-Screen.html";
 }
 
